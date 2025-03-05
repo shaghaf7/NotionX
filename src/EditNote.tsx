1ac77f347e73a5e4929b86e84bc92459eed60709
shaghaf7 <shaghafkashif@gmail.com>
2025-03-05 23:24:24 +0530

    EditNote section added

---
+++ src/EditNote.tsx
@@ -0,0 +1,26 @@
+import type { NoteData, Tag } from "./App";
+import { NoteForm } from "./NoteForm";
+import { useNote } from "./NoteLayout.tsx";
+
+type EditNoteProps = {
+  onSubmit: (id: string, data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export function EditNote({ onSubmit, onAddTag, availableTags }: EditNoteProps) {
+  const note = useNote(); // Ensure `useNote()` is returning a valid `Note` object
+  return (
+    <>
+      <h1 className="mb-4">Edit Note</h1>
+      <NoteForm
+        title={note.title}
+        markdown={note.markdown}
+        tags={note.tags}
+        onSubmit={(data) => onSubmit(note.id, data)}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  );
+}
